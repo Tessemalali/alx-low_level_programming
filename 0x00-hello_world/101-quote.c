12f010ac1cf1f2b1d0b3c92a831ff26f69bea848
Tessema <tessemamineo@yahoo.com>
2022-12-08 08:37:27 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 #include <unistd.h>
 
 /**
- * main - Entry piont
+ * main - prints exactly and that piece of art is useful
  *
- * Retrun: 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
