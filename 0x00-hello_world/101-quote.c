8a64528059d3b6e1a303d1af07d7e491668a66b6
Tessema <tessemamineo@yahoo.com>
2022-12-08 07:45:17 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
- * main - Entry point
+
+/**
+ * main - Entry piont
  *
- * Return: Always 1 (Success)
+ * Retrun: 1 (Success)
  */
- 
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is unseful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-
-} 
+}
