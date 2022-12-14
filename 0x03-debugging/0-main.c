d14e85c8701811db5399d8ae65f6247bb42f2684
Tessema <tessemamineo@yahoo.com>
2022-12-14 00:29:24 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - causes an infinite loop
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        postive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
-
