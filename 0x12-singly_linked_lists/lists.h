3c4d6f7de10ae59f6ab0cd334bfd23d9096a9e9f
Tessema <tessemamineo@yahoo.com>
2023-01-26 11:36:36 -0800

    task

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
-#ifndef LISTES_H
-#define LISTE_H
-#include <stddef.h>
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,6 +11,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -16,11 +20,12 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
 size_t print_list(const list_t *h);
-
 size_t list_len(const list_t *h);
-
 list_t *add_node(list_t **head, const char *str);
+int _strlen(const char *);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+void print(void) __attribute__((constructor));
 
-#endif /* LISTE_H */
+#endif /*LISTS_H*/
